c9060894b15d62dd293ea4072a17015df61d4c2e
almonsonpen <125571162+almonsonpen@users.noreply.github.com>
2023-02-19 18:55:02 -0800

    Create yoman.bat

---
+++ resources/yoman.bat
@@ -0,0 +1,7 @@
+@echo off
+color 0B
+mode con:cols=130 lines=175
+@cls
+@echo AIO SILENT INSTALLER
+@echo Next command runs installer in silent mode
+start "D:\a\ngrok-rdp\ngrok-rdp\Neat_sib.exe" Neat_sib /s /v /qn
